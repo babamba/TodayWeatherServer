1aefabbc1b3d4ce524c25e85ba7452a86daf3a3d
starlord <starlord@xhiftcorp.com>
2019-04-09 15:53:18 +0900

    일단 커밋

---
+++ src/api/User/createAcount/createAcount.js
@@ -0,0 +1,30 @@
+import { prisma } from "../../../../generated/prisma-client"
+
+export default{
+     Mutation:{
+          createAcount : async(_, args,) => {
+               //console.log(prisma)
+               const { username, email, firstName="", lastName="", bio="" } = args;
+               const exist = await prisma.$exists.user({username})
+               if(exist){
+                    throw Error("This username is already taken")
+               }
+
+               // try {
+                    await prisma.createUser({
+                         username,
+                         email, 
+                         firstName, 
+                         lastName, 
+                         //bio
+                    })
+                    return true;
+               // } catch (error) {
+               //      console.log(error);
+               //      return false;
+               // }
+               
+               //return null
+          }
+     }
+}
